951292070eb94742e4c334533c0da96fc3ad2401
Lim Chee Aun <cheeaun@gmail.com>
2022-02-27 00:49:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 👷 gh-starred-repos-feed
+
+A Cloudflare Worker that returns the starred repos of a GitHub user as a RSS feed
+
+## Technicalities
+
+- `wrangler dev` - start local development server
---
+++ index.js
@@ -0,0 +1,77 @@
+import { Feed } from 'feed';
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  // Atom one is buggy for now
+  // const pathRegex = /\/([^/]+)\.(rss|atom|json)/i;
+  const pathRegex = /\/([^/]+)\.(rss|json)/i;
+  const [_, username, format] = url.pathname.match(pathRegex) || [];
+  if (format) {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/starred`,
+      {
+        headers: {
+          Accept: 'Accept: application/vnd.github.v3+json',
+          'User-Agent': 'gh-starred-repos-feed/1.0',
+        },
+      },
+    );
+    const json = await response.json();
+    const feed = new Feed({
+      title: `${username}'s starred repos on GitHub`,
+      description: `A feed of ${username}'s starred repos on GitHub`,
+      link: `https://github.com/stars/${username}/`,
+      generator: 'gh-starred-repos-feed/1.0',
+    });
+    json.forEach(repo => {
+      feed.addItem({
+        title: repo.name,
+        id: repo.id,
+        link: repo.html_url,
+        description: repo.description,
+        content: `<p>${repo.description}</p><p>⭐ ${repo.stargazers_count} 🔱 ${repo.forks_count}</p>`,
+        author: [
+          {
+            name: repo.owner.login,
+            link: repo.owner.html_url,
+          },
+        ],
+      });
+    });
+    const feedMapping = {
+      rss: {
+        type: 'application/rss+xml',
+        fn: 'rss2',
+      },
+      // atom: {
+      //   type: 'application/atom+xml',
+      //   fn: 'atom1',
+      // },
+      json: {
+        type: 'application/json',
+        fn: 'json1',
+      },
+    };
+    const feedResponse = feed[feedMapping[format].fn]();
+    return new Response(feedResponse, {
+      headers: {
+        'content-type': `${feedMapping[format].type}; charset=utf-8`,
+        'cache-control': 'public, max-age=60, s-maxage=60',
+      },
+    });
+  }
+
+  if (url.pathname === '/') {
+    return new Response(
+      'Check out the repo: https://github.com/cheeaun/gh-starred-repos-feed',
+    );
+  }
+
+  return new Response('Page not found', {
+    status: 404,
+  });
+}
